9d0d4951e1f8fef2a3c648cc893867df0de84a4c
rudrabarad <rudra.barad@gmail.com>
2020-05-09 00:29:47 +0530

    12:30 AM | 09-05-20

---
+++ Shapes And Text/drawing_circle.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+background = np.zeros((500, 500, 4), dtype='uint8')
+# to create a black background .zeros((pt1,pt1,channels), dtype='8-bit signed integer')
+
+cv2.circle(background, (250, 250), 200, (225, 225, 225), 1, lineType=8, shift=0)
+# .circle(pic,center,radius,color,thickness.line-type,shift(decrease))
+
+cv2.imshow('circle', background)
+# .image(name of window, object to show)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ Shapes And Text/drawing_line.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+background = np.zeros((500, 500, 4), dtype='uint8')
+# to create a black background .zeros((pt1,pt1,channels), dtype='8-bit signed integer')
+
+cv2.line(background, (250, 250), (450, 450), (225, 225, 225), 1, lineType=8, shift=0)
+# .line(pic,pt1,pt2,color,thickness.line-type,shift(decrease))
+
+cv2.imshow('line', background)
+# .image(name of window, object to show)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
