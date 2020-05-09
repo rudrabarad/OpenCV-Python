15c8d213f2135369cb7014e2c6cc8ad05b4b4a6c
rudrabarad <rudra.barad@gmail.com>
2020-05-09 08:51:28 +0530

    09:00 AM | 09/05/20

--- Image Fundamentals/change_format.py
+++ Image Fundamentals/change_format.py
@@ -2,7 +2,7 @@ import numpy as np      # used to handle all the multi dimensional arrays within
 import cv2
 
 img = cv2.imread('scene_jpg.jpg', 0)     # used to read an grey image
-img = cv2.imwrite('scene_png.png', img)   # to save image in png format with grey color
+img = cv2.imwrite('PNG Format/scene_png.png', img)   # to save image in png format with grey color in subfolder
 cv2.imshow('Original', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
